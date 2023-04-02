e705c27957e4da04ea983d1ad484ddbde970c398
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-04-02 02:48:28 -0700

    Update 🌎_Visualization_with_NumPy.py

--- 🌎_Visualization_with_NumPy.py
+++ 🌎_Visualization_with_NumPy.py
@@ -11,7 +11,7 @@ st.write("<div align='left'><span style='color:#FF5A5F; font-size: 15px;'>Amster
 
 st.markdown(
     "<div style='display: flex; align-items: left;'>"
-    "<h1 style='color:#484848; margin-right=-40px'>Visualization with</h1>"
+    "<h1 style='color:#484848; margin-right: -40px'>Visualization with</h1>"
     "<h1 style='color:#FF5A5F;'>NumPy 🗺️</h1>"
     "</div>", 
     unsafe_allow_html=True
