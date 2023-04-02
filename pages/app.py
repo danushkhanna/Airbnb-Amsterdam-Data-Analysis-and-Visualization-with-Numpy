3759ae0ddd9d8622bccadcac34f707ac5769e685
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-04-01 23:09:37 -0700

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -73,8 +73,8 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                 user_date_input = right.date_input(
                     f"Values for {column}",
                     value=(
-                        df[column].min(),
-                        df[column].max(),
+                        df[column].min().date(),
+                        df[column].max().date(),
                     ),
                 )
                 if len(user_date_input) == 2:
@@ -91,6 +91,5 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-df = pd.read_csv("Airbnb-Amsterdam-Data-Analysis-and-Visualization-with-Numpy/pages/WK2_Airbnb_Amsterdam_listings_proj_solution.csv", index_col=0
-)
+df = pd.read_csv("https://raw.githubusercontent.com/danushkhanna/Airbnb-Amsterdam-Data-Analysis-and-Visualization-with-Numpy/main/pages/WK2_Airbnb_Amsterdam_listings_proj_solution.csv", index_col=0)
 st.dataframe(filter_dataframe(df))
