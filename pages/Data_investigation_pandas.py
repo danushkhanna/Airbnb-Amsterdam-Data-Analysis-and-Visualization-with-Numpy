ea3adc34a097e6becfe134a069f7860b986f3730
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-04-01 23:40:57 -0700

    Update Data_investigation_pandas.py

--- pages/Data_investigation_pandas.py
+++ pages/Data_investigation_pandas.py
@@ -9,8 +9,6 @@ from pandas.api.types import (
 
 st.title("Filter your Airbnb Listings dataframe!")
 
-st.set_page_config(page_title="Filter your Airbnb Listings dataframe!")
-
 st.write(
     """This app is based on this blog [here](https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/). 
     Can you think of ways to extend it with visuals?
