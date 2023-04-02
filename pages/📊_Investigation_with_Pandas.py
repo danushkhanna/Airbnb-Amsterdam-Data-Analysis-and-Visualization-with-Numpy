95648541ba828555f6cb5445240ea2dc95e523f7
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-04-02 02:42:09 -0700

    Update 📊_Investigation_with_Pandas.py

--- pages/📊_Investigation_with_Pandas.py
+++ pages/📊_Investigation_with_Pandas.py
@@ -16,8 +16,8 @@ st.write("<div align='left'><span style='color:#FF5A5F; font-size: 15px;'>Amster
 
 st.markdown(
     "<div style='display: flex; align-items: center;'>"
-    "<h1 style='color:#484848; margin-right: 20px; margin-bottom: 0px;'>Investigation with</h1>"
-    "<h1 style='color:#FF5A5F; margin-bottom: 0px;'>Pandas&nbsp;📈</h1>"
+    "<h1 style='color:#484848; margin-bottom: 0px; font-size: 48px;'>Investigation with</h1>"
+    "<h1 style='color:#FF5A5F; margin-bottom: 0px; font-size: 64px;'>Pandas&nbsp;📈</h1>"
     "</div>", 
     unsafe_allow_html=True
 )
